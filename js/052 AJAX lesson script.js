1395017d73bab285f513532215b563c74cbc5303
Margarita <roppeldmargo@gmail.com>
2021-02-26 19:11:29 +0200

    some setting mode

--- js/052 AJAX lesson script.js
+++ js/052 AJAX lesson script.js
@@ -4,3 +4,11 @@
 
 // 1 вариант AJAX XML
 
+    const inputRub = document.querySelector('#rub'),
+          inputUsd = document.querySelector('#usd');
+
+    inputRub.addEventListener('input', () => {
+        const request = new XMLHttpRequest();
+
+        request.open();//этот метод не открывает соединение между фронтендом или бэкэндом, а лишь собирает настройки, которые помогут в будущем сделать запрос
+    });
